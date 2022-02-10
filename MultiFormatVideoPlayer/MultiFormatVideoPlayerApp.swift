3c98a94e0e1170ca5eaf6f1a212a154dd95575ee
Jing Wei Li <ljw9801055@gmail.com>
2022-02-09 22:49:11 -0500

    Initial Commit

---
+++ MultiFormatVideoPlayer/MultiFormatVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiFormatVideoPlayerApp.swift
+//  MultiFormatVideoPlayer
+//
+//  Created by Jing Wei Li on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiFormatVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
